10c63453659d99a8fb7ab3146a91bc07c7413c75
eliashermon <eliashermon419@gmail.com>
2024-09-13 01:10:25 -0500

    netflix-clone

---
+++ Netflix-Clone/src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+import Banner from './compononts/Banner/Banner'
+import Header from './compononts/Header/Header'
+
+
+import Footer from './compononts/Footer/Footer'
+import RowList from './compononts/Rows/RowList'
+
+
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+       <div>
+      <Header/>
+      <Banner/>
+      <RowList/>
+    <Footer/>
+    
+    
+
+       </div>
+       </>
+  )
+}
+
+export default App
